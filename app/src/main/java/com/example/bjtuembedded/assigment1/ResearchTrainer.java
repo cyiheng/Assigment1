e315863c8eea41d8b46983f3a84e85ccbb231b93
cyiheng <cyiheng@users.noreply.github.com>
2018-11-07 17:22:09 +0800

    Add booking activity
    Should put a fragment or dialogAlert and not activity
    Maybe check how to use the calendar

--- app/src/main/java/com/example/bjtuembedded/assigment1/ResearchTrainer.java
+++ app/src/main/java/com/example/bjtuembedded/assigment1/ResearchTrainer.java
@@ -1,14 +1,28 @@
 package com.example.bjtuembedded.assigment1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ResearchTrainer extends AppCompatActivity {
 
+    private Button aButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.research_trainer);
+
+        aButton =(Button)findViewById(R.id.button2);
+
+        aButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent vIntent = new Intent(ResearchTrainer.this, TrainerInfo.class);
+                startActivity(vIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/bjtuembedded/assigment1/TrainerInfo.java
+++ app/src/main/java/com/example/bjtuembedded/assigment1/TrainerInfo.java
@@ -1,13 +1,25 @@
 package com.example.bjtuembedded.assigment1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TrainerInfo extends AppCompatActivity {
-
+    private Button aBook;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.trainer_info);
+
+        aBook = (Button)findViewById(R.id.trainer_book);
+        aBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent vIntent = new Intent(TrainerInfo.this,BookingScreen.class);
+                startActivity(vIntent);
+            }
+        });
     }
 }
